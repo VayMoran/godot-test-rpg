07c66097debc70f9df15536b9a4c28ffe07145a6
YulinAle <smoran0990@gmail.com>
2018-01-13 18:41:25 -0600

    Removed unnecessary files.

--- character.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="character" tilewidth="16" tileheight="16" tilecount="272" columns="17">
- <image source="character.png" width="272" height="256"/>
-</tileset>
