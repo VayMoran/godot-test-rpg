d6dc27d5cd08bed602ca5a3e71bb58bc42b4aaea
YulinAle <smoran0990@gmail.com>
2018-01-13 18:30:31 -0600

    Uploading bas project

---
+++ lpc_terrain.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lpc_terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="terrain_atlas.png" width="1024" height="1024"/>
+ <terraintypes>
+  <terrain name="grass_transparent" tile="277"/>
+  <terrain name="sand_light_transparent" tile="79"/>
+  <terrain name="water_grass" tile="358"/>
+ </terraintypes>
+ <tile id="79" terrain=",,,1"/>
+ <tile id="80" terrain=",,1,1"/>
+ <tile id="81" terrain=",,1,"/>
+ <tile id="111" terrain=",1,,1"/>
+ <tile id="112" terrain="1,1,1,1"/>
+ <tile id="113" terrain="1,,1,"/>
+ <tile id="143" terrain=",1,,"/>
+ <tile id="144" terrain="1,1,,"/>
+ <tile id="145" terrain="1,,,"/>
+ <tile id="277" terrain=",,,0"/>
+ <tile id="278" terrain=",,0,0"/>
+ <tile id="279" terrain=",,0,"/>
+ <tile id="309" terrain=",0,,0"/>
+ <tile id="310" terrain="0,0,0,0"/>
+ <tile id="311" terrain="0,,0,"/>
+ <tile id="341" terrain=",0,,"/>
+ <tile id="342" terrain="0,0,,"/>
+ <tile id="343" terrain="0,,,"/>
+ <tile id="358" terrain=",,,2"/>
+ <tile id="359" terrain=",,2,2"/>
+ <tile id="360" terrain=",,2,"/>
+ <tile id="390" terrain=",2,,2"/>
+ <tile id="391" terrain="2,2,2,2"/>
+ <tile id="392" terrain="2,,2,"/>
+ <tile id="422" terrain=",2,,"/>
+ <tile id="423" terrain="2,2,,"/>
+ <tile id="424" terrain="2,,,"/>
+</tileset>
